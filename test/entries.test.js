e0106187673b732ccebe28cad6cd3493c1c8a297
Chris Hafey <chafey@Chriss-MBP.localdomain>
2026-02-04 22:10:56 -0600

    feat: add suport for entries() returning results in descending order

--- test/entries.test.js
+++ test/entries.test.js
@@ -242,4 +242,145 @@ describe('entries', () => {
       assert.equal(results[i][0], key)
     }
   })
+
+  it('lists entries in ascending order (explicit)', async () => {
+    const empty = await ShardBlock.create()
+    const blocks = new Blockstore()
+    await blocks.put(empty.cid, empty.bytes)
+
+    /** @type {Array<[string, API.UnknownLink]>} */
+    const testdata = [
+      ['c', await randomCID(32)],
+      ['d', await randomCID(32)],
+      ['a', await randomCID(32)],
+      ['b', await randomCID(32)]
+    ]
+
+    /** @type {API.ShardLink} */
+    let root = empty.cid
+    for (const [k, v] of testdata) {
+      const res = await put(blocks, root, k, v)
+      for (const b of res.additions) {
+        await blocks.put(b.cid, b.bytes)
+      }
+      root = res.root
+    }
+
+    const results = []
+    for await (const entry of entries(blocks, root, { order: 'asc' })) {
+      results.push(entry)
+    }
+
+    const sortedKeys = testdata.map(d => d[0]).sort()
+    for (const [i, key] of sortedKeys.entries()) {
+      assert.equal(results[i][0], key)
+    }
+  })
+
+  it('lists entries in descending order', async () => {
+    const empty = await ShardBlock.create()
+    const blocks = new Blockstore()
+    await blocks.put(empty.cid, empty.bytes)
+
+    /** @type {Array<[string, API.UnknownLink]>} */
+    const testdata = [
+      ['c', await randomCID(32)],
+      ['d', await randomCID(32)],
+      ['a', await randomCID(32)],
+      ['b', await randomCID(32)]
+    ]
+
+    /** @type {API.ShardLink} */
+    let root = empty.cid
+    for (const [k, v] of testdata) {
+      const res = await put(blocks, root, k, v)
+      for (const b of res.additions) {
+        await blocks.put(b.cid, b.bytes)
+      }
+      root = res.root
+    }
+
+    const results = []
+    for await (const entry of entries(blocks, root, { order: 'desc' })) {
+      results.push(entry)
+    }
+
+    const sortedKeys = testdata.map(d => d[0]).sort().reverse()
+    for (const [i, key] of sortedKeys.entries()) {
+      assert.equal(results[i][0], key)
+    }
+  })
+
+  it('lists entries by prefix in descending order', async () => {
+    const empty = await ShardBlock.create()
+    const blocks = new Blockstore()
+    await blocks.put(empty.cid, empty.bytes)
+
+    /** @type {Array<[string, API.UnknownLink]>} */
+    const testdata = [
+      ['dccc', await randomCID(32)],
+      ['deee', await randomCID(32)],
+      ['dooo', await randomCID(32)],
+      ['daaa', await randomCID(32)],
+      ['beee', await randomCID(32)]
+    ]
+
+    /** @type {API.ShardLink} */
+    let root = empty.cid
+    for (const [k, v] of testdata) {
+      const res = await put(blocks, root, k, v)
+      for (const b of res.additions) {
+        await blocks.put(b.cid, b.bytes)
+      }
+      root = res.root
+    }
+
+    const prefix = 'd'
+    const results = []
+    for await (const entry of entries(blocks, root, { prefix, order: 'desc' })) {
+      results.push(entry)
+    }
+
+    const filteredAndSorted = testdata.map(d => d[0]).filter(k => k.startsWith(prefix)).sort().reverse()
+    for (const [i, key] of filteredAndSorted.entries()) {
+      assert.equal(results[i][0], key)
+    }
+  })
+
+  it('lists entries by range in descending order', async () => {
+    const empty = await ShardBlock.create()
+    const blocks = new Blockstore()
+    await blocks.put(empty.cid, empty.bytes)
+
+    /** @type {Array<[string, API.UnknownLink]>} */
+    const testdata = [
+      ['aaaa', await randomCID(32)],
+      ['cccc', await randomCID(32)],
+      ['deee', await randomCID(32)],
+      ['dooo', await randomCID(32)],
+      ['eeee', await randomCID(32)]
+    ]
+
+    /** @type {API.ShardLink} */
+    let root = empty.cid
+    for (const [k, v] of testdata) {
+      const res = await put(blocks, root, k, v)
+      for (const b of res.additions) {
+        await blocks.put(b.cid, b.bytes)
+      }
+      root = res.root
+    }
+
+    const gte = 'c'
+    const lt = 'e'
+    const results = []
+    for await (const entry of entries(blocks, root, { gte, lt, order: 'desc' })) {
+      results.push(entry)
+    }
+
+    const filteredAndSorted = testdata.map(d => d[0]).filter(k => k >= gte && k < lt).sort().reverse()
+    for (const [i, key] of filteredAndSorted.entries()) {
+      assert.equal(results[i][0], key)
+    }
+  })
 })
